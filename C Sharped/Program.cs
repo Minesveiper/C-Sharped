a61927972e2a018f7bbe38c29da0009b1db52225
Minesveiper <ASNilsen@gmail.com>
2022-12-14 13:06:26 +0100

    Add project files.

---
+++ C Sharped/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharped
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //---------------------Diamond------------------------------------------------
+            /*
+            Console.WriteLine("Display the pattern like diamond:");
+            Console.WriteLine("----------------------------------\n");
+            Console.Write("Input number of rows (All of the diamond): "); // I wrote it to account for all of the diamond.
+            int diamondRows = int.Parse(Console.ReadLine());
+            int k = diamondRows/2;
+            Console.WriteLine("\n");
+
+            for (int i = 1; i < k; i++)     //Increase Loop
+            {
+                for (int j = i; j < k; j++)
+                {
+                    Console.Write(" ");     // spaces
+                }
+                for (int h = 0; h < i*2-1; h++)
+                {
+                    Console.Write("*");     //stars
+                }
+                Console.WriteLine();
+            }
+            for (int i = k; i > 0; i--)     //Decrease Loop
+            {
+                for (int j = i; j < k; j++)
+                {
+                    Console.Write(" ");     // spaces
+                }
+                for (int h = 0; h < i * 2 - 1; h++)
+                {
+                    Console.Write("*");     //stars
+                }
+                Console.WriteLine();
+            }
+            */
+            //----------------------------------------------------------------------------
+
+            //----------------------------------------------------------------------------
+
+
+
+
+            //-------------Substring------------------------------------------------------
+            /*
+            string strang = "Rock'n roll is good for your soul";
+            bool subs = strang.Contains("roll is good");
+            Console.WriteLine(subs);
+            */
+            //----------------------------------------------------------------------------
+
+            //-------------Fibonacci Numbers----------------------------------------------
+            //Fibonacci(8);
+            //Fibonacci(15);
+            //----------------------------------------------------------------------------
+
+        }
+
+        private static void Fibonacci(int number)
+        {
+            int a = 0; int b = 1; int c = 0;
+            for (int i = 0; i < number; i++)
+            {
+                c = a + b;
+                Console.Write($"{c} " );
+                a = b;
+                b = c;
+
+            }
+            Console.WriteLine($" - End Calculation of Fibonacci numbers at {number}.");
+        }
+    }
+}
+
---
+++ C Sharped/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharped
+{
+    internal class Vehicle
+    {
+        public string Colour { get; set; }  //colour
+        public string Manufacturer { get; set; }    //manufacturer
+        public string GasLevel { get; set; }    //gas level
+
+        //Drive()
+    }
+
+    internal class Car : Vehicle
+    {
+        //OpenWindow()
+        //OpenRadio()
+    }
+
+    internal class Ecar : Vehicle 
+    {
+        //Override Drive()
+    }
+
+    internal class Motorcycle : Vehicle
+    {
+        //PutHelmetOn()
+    }
+}
